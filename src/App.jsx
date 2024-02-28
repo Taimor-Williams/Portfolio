112ebcc8f6314664d71e8dc0936974554490f1e2
Taimor Williams <taimor@mit.edu>
2024-02-28 13:08:14 -0500

    got navBar mostly configured like I want, going to work on the dropdown menu now

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,8 @@ import Scroll from './components/Scroll';
 
 function App() {
   return (
-    <div class="pt-8 text-base font-semibold leading-7">
-      <h1 className = "text-center text-sky-500 hover:text-sky-600">
-          Hello World
-      </h1>
-      <p className='text-left text-stone-900'>
-        This is my resume. It is amazing. You will see my power!!!
-      </p>
-      <Scroll></Scroll>
+    <div>
+      <Scroll className ="py-16"></Scroll>
     </div>
   );
 }
--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,15 +1,15 @@
 import Button from "./Button";
 import reactLogo from '../../public/logo192.png'
 
-const sections = ["hello", "world", "death", "my enemies", "more"];
+const sections = ["hello", "world", "death", "my enemies", "more", "ivonne", "me"];
 
 
 export default function Scroll(){
 
     return(
-        <nav className="w-full items-center bg-sky-600 ring-4 ring-sky-800 hover:ring-sky-300">
+        <nav className="mt-4 w-full bg-sky-600 ring-4 ring-sky-800 hover:ring-sky-300">
             {/* <h2 className="mb-8 font-bold uppercase justify-between px-3 md:text-xl text-stone-200">Your Projects</h2> */}
-            <div className="flex justify-between items-center">
+            <div className="px-4 flex justify-between items-center">
                 <div className="flex items-center">
                     <h2 className="text-stone-100 px-4">Portfolio</h2>
                     <img src = {reactLogo} className="h-8 w-8"></img>
